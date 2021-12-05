e624dfc002dcdbf3d38b482ec9e19fe59962c6fe
MrWhiteShapi <aigunovshapi@yandex.ru>
2021-12-05 21:04:34 +0300

    first commit

---
+++ src/com/company/somjava.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class somjava {
+    public static void main() {
+        System.out.println(printWatashiKira());
+    }
+    public static String printWatashiKira(){
+        String monolog = "Меня зовут Кира Йошикаге. Мне 33 года. " +
+                "Мой дом находится в северо-восточной части Морио, где расположены все виллы. Я не женат. " +
+                "Я работаю в универмаге Kame Yu и прихожу домой не позднее 8 вечера." +
+                " Я не курю, но иногда выпиваю. " +
+                "Я ложусь спать в 11 вечера, и убеждаюсь, что я получаю ровно восемь часов сна, несмотря ни на что";
+        return monolog;
+    }
+}
